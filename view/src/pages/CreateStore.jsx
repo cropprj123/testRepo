fe5f975f6a60b6472f3bc612eabb32656fdd093a
ompatil7 <ompatil4749@gmail.com>
2024-08-05 23:31:50 +0530

    mod 3

--- view/src/pages/CreateStore.jsx
+++ view/src/pages/CreateStore.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
